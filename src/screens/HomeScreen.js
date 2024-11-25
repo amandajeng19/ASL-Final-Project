acd4177ca8661ee44696fe2076dc4016c39d2fe9
amandajeng19 <amandajeng19@gmail.com>
2024-11-24 18:13:57 -0800

    UI is roughly in place.

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,19 +1,51 @@
-import { StyleSheet, SafeAreaView, Text } from 'react-native';
+import { StyleSheet, SafeAreaView, Text, TouchableOpacity, Image, View } from 'react-native';
 
 
-export default function HomeScreen({ navigation }) {
-    return (
-		<SafeAreaView style={styles.container}>
-			<Text>This is the HomeScreen.</Text>
-		</SafeAreaView>
-	);
+export default function WelcomeScreen({ navigation }) {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={[styles.cameraFeed, {flex: 1}]}></View>
+      <TouchableOpacity 
+        style={styles.button}
+        onPress={() => navigation.navigate("Home")}>
+          <View style={styles.icon}></View>
+          <Text style={styles.buttonText}>Flip Camera</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    backgroundColor: '#fff',
+  },
+  cameraFeed: {
+    width: '94%',
+    height: 'auto',
+    backgroundColor: "#000",
+  },
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 16,
+    paddingHorizontal: 24,
+    paddingVertical: 10,
+    backgroundColor: '#d6d6d6',
+  },
+  buttonText: {
+    textAlign: "center",
+    fontSize: 24,
+    fontWeight: "semibold",
+    color: "#000",
+  },
+  icon: {
+    width: 40,
+    height: 40,
+    marginRight: 16,
+    backgroundColor: "#000",
+  }
+});
--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,14 +1,16 @@
-import { StyleSheet, SafeAreaView, Text, Button } from 'react-native';
+import { StyleSheet, SafeAreaView, Text, TouchableOpacity } from 'react-native';
 
 
 export default function WelcomeScreen({ navigation }) {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>This is the WelcomeScreen.</Text>
-      <Button
-        title="Go to Home Screen"
-        onPress={() => navigation.navigate("Home")}
-      />
+      <Text style={styles.title}>American Sign Language</Text>
+      <Text style={styles.header}>Finger-spelling Translator</Text>
+      <TouchableOpacity 
+        style={styles.button}
+        onPress={() => navigation.navigate("Home")}>
+          <Text style={styles.buttonText}>Let’s go!</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
@@ -16,8 +18,35 @@ export default function WelcomeScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  title: {
+    textAlign: "center",
+    marginVertical: 20,
+    fontSize: 40,
+    fontWeight: "bold",
+    color: "#000",
+  },
+  header: {
+    textAlign: "center",
+    marginBottom: 32,
+    fontSize: 24,
+    fontWeight: "medium",
+    color: "#000",
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 24,
+    paddingVertical: 10,
+    backgroundColor: '#d6d6d6',
+  },
+  buttonText: {
+    textAlign: "center",
+    fontSize: 24,
+    fontWeight: "semibold",
+    color: "#000",
   },
 });
